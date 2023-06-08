3f67c75c86febee825a01390715499826c95c190
Zeyang Zhang <zeyangz2@illinois.edu>
2023-06-08 11:37:40 -0500

    LSTM version1

---
+++ core/data_processor.py
@@ -0,0 +1,73 @@
+import math
+import numpy as np
+import pandas as pd
+
+class DataLoader():
+
+    def __init__(self, filename, split, cols):
+        dataframe = pd.read_csv(filename)
+        i_split = int(len(dataframe) * split)
+        self.data_train = dataframe.get(cols).values[:i_split]
+        self.data_test  = dataframe.get(cols).values[i_split:]
+        self.len_train  = len(self.data_train)
+        self.len_test   = len(self.data_test)
+        self.len_train_windows = None
+
+    def get_test_data(self, seq_len, normalise):
+
+        data_windows = []
+        for i in range(self.len_test - seq_len):
+            data_windows.append(self.data_test[i:i+seq_len])
+
+        data_windows = np.array(data_windows).astype(float)
+        data_windows = self.normalise_windows(data_windows, single_window=False) if normalise else data_windows
+
+        x = data_windows[:, :-1]
+        y = data_windows[:, -1, [0]]
+        return x,y
+
+    def get_train_data(self, seq_len, normalise):
+
+        data_x = []
+        data_y = []
+        for i in range(self.len_train - seq_len):
+            x, y = self._next_window(i, seq_len, normalise)
+            data_x.append(x)
+            data_y.append(y)
+        return np.array(data_x), np.array(data_y)
+
+    def generate_train_batch(self, seq_len, batch_size, normalise):
+        
+        i = 0
+        while i < (self.len_train - seq_len):
+            x_batch = []
+            y_batch = []
+            for b in range(batch_size):
+                if i >= (self.len_train - seq_len):
+                    # stop-condition for a smaller final batch if data doesn't divide evenly
+                    yield np.array(x_batch), np.array(y_batch)
+                    i = 0
+                x, y = self._next_window(i, seq_len, normalise)
+                x_batch.append(x)
+                y_batch.append(y)
+                i += 1
+            yield np.array(x_batch), np.array(y_batch)
+
+    def _next_window(self, i, seq_len, normalise):
+        window = self.data_train[i:i+seq_len]
+        window = self.normalise_windows(window, single_window=True)[0] if normalise else window
+        x = window[:-1]
+        y = window[-1, [0]]
+        return x, y
+
+    def normalise_windows(self, window_data, single_window=False):
+        normalised_data = []
+        window_data = [window_data] if single_window else window_data
+        for window in window_data:
+            normalised_window = []
+            for col_i in range(window.shape[1]):
+                normalised_col = [((float(p) / float(window[0, col_i])) - 1) for p in window[:, col_i]]
+                normalised_window.append(normalised_col)
+            normalised_window = np.array(normalised_window).T 
+            normalised_data.append(normalised_window)
+        return np.array(normalised_data)
---
+++ run.py
@@ -0,0 +1,93 @@
+import os
+import json
+import time
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+from core.data_processor import DataLoader
+from core.model import Model
+from keras.utils import plot_model
+
+# 绘图展示结果
+def plot_results(predicted_data, true_data):
+    fig = plt.figure(facecolor='white')
+    ax = fig.add_subplot(111)
+    ax.plot(true_data, label='True Data')
+    plt.plot(predicted_data, label='Prediction')
+    plt.legend()
+    #plt.show()
+    plt.savefig('results_2_try.png')
+
+
+def plot_results_multiple(predicted_data, true_data, prediction_len):
+    fig = plt.figure(facecolor='white')
+    ax = fig.add_subplot(111)
+    ax.plot(true_data, label='True Data')
+    plt.legend()
+    for i, data in enumerate(predicted_data):
+        padding = [None for p in range(i * prediction_len)]
+        plt.plot(padding + data, label='Prediction')
+    #plt.show()
+    plt.savefig('results_multiple_try.png')
+
+#RNN时间序列
+#RNN
+def main():
+    #读取所需参数
+    #get hyperparameters from json
+    configs = json.load(open('config_2.json', 'r'))
+    if not os.path.exists(configs['model']['save_dir']): os.makedirs(configs['model']['save_dir'])
+    #读取数据
+    #get data from csv
+    data = DataLoader(
+        os.path.join('data', configs['data']['filename']),
+        configs['data']['train_test_split'],
+        configs['data']['columns']
+    )
+    #创建RNN模型
+    #make a RNN model error with showing the model in picture
+    model = Model()
+    mymodel = model.build_model(configs)
+    
+    # plot_model(mymodel, to_file='model.png',show_shapes=True)
+    
+    #加载训练数据
+    #get normalized data
+    x, y = data.get_train_data(
+        seq_len=configs['data']['sequence_length'],
+        normalise=configs['data']['normalise']
+    )
+    print (x.shape)
+    print (y.shape)
+    
+	#训练模型
+    #train LSTM model
+    model.train(
+		x,
+		y,
+		epochs = configs['training']['epochs'],
+		batch_size = configs['training']['batch_size'],
+		save_dir = configs['model']['save_dir']
+	)
+	
+   #测试结果
+    #test
+    x_test, y_test = data.get_test_data(
+        seq_len=configs['data']['sequence_length'],
+        normalise=configs['data']['normalise']
+    )
+    
+    #展示测试效果
+    predictions_multiseq = model.predict_sequences_multiple(x_test, configs['data']['sequence_length'], configs['data']['sequence_length'])
+    predictions_pointbypoint = model.predict_point_by_point(x_test,debug=True)
+
+    # print(predictions_pointbypoint)
+    np.save('x.npy', predictions_pointbypoint)
+    # print(y_test)
+    # np.save('z_test.npy', y_test)
+    
+    plot_results_multiple(predictions_multiseq, y_test, configs['data']['sequence_length'])
+    plot_results(predictions_pointbypoint, y_test)
+    
+if __name__ == '__main__':
+    main()
